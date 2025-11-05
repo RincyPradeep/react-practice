ab7b9c2827c02242a978516d81357aa6c1aacb3f
Rincy Pradeep <rincypradeep15@gmail.com>
2025-11-05 15:39:48 +0530

    multi step form with typescript and shadcn

---
+++ src/02_multi_step_form/MultiStepForm.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export const MultiStepForm = () => {
+  return (
+    <div>
+        <Button variant={"outline"}>Next</Button>
+    </div>
+  )
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+
+// @ts-ignore
 import CheckoutStepper from './01_stepper/CheckoutStepper'
+
+import { MultiStepForm } from './02_multi_step_form/MultiStepForm'
+
 
 function App() {
   return (
     <>
-      <CheckoutStepper />
+      {/* <CheckoutStepper /> */}
+      <MultiStepForm />
         
     </>
   )
